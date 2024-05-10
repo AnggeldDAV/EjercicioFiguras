351621a9f05a630854e4f1d8338dd0850cb62f8f
AnggeldDAV <anggeld.adav@gmail.com>
2024-05-10 12:16:30 +0200

    TableroFiguras

---
+++ EjercicioFiguras/Modelo/TableroFiguras.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioFiguras.Modelo
+{
+    public class TableroFiguras
+    {
+        public double SuperficieTotal;
+        public double PerimetroTotal;
+        public List<IMedible> Tablero = new();
+        public void Add(IMedible elemento)
+        {
+            this.Tablero.Add(elemento);
+        }
+        public int Count()
+        {
+            return this.Tablero.Count();
+        }
+        public double DamePerimetroTotal()
+        {
+            PerimetroTotal = 0;
+            Parallel.ForEach(Tablero, number =>
+            {
+                PerimetroTotal += number.DamePerimetro();
+            });
+            return PerimetroTotal;
+        }
+        public double DameSuperficieTotal()
+        {
+            SuperficieTotal = 0;
+            Parallel.ForEach(Tablero, number =>
+            {
+                SuperficieTotal += number.DameSuperficie();
+            });
+            return SuperficieTotal;
+        }
+        public double DameMediaTotalPerimetro()
+        {
+            return (this.DamePerimetroTotal()) / this.Count();
+        }
+        public double DameMediaTotalSuperficie()
+        {
+            return (this.DameSuperficieTotal()) / this.Count();
+        }
+        public double DameSuperficieGrande()
+        {
+            double Temp = 0;
+            Parallel.ForEach(Tablero, number =>
+            {
+                if (number.DameSuperficie() > Temp)
+                {
+                    Temp = number.DameSuperficie();
+                }
+            });
+            return Temp;
+
+        }
+        public double DameSuperficiePequena()
+        {
+            double Temp = 1000000000000000000;
+            Parallel.ForEach(Tablero, number =>
+            {
+                if (number.DameSuperficie() < Temp)
+                {
+                    Temp = number.DameSuperficie();
+                }
+            });
+            return Temp;
+
+        }
+    }
+}
--- EjercicioFiguras/Program.cs
+++ EjercicioFiguras/Program.cs
@@ -4,7 +4,28 @@ using EjercicioFiguras.Modelo;
 IFactoriaMedibles Factoria = new FactoriaFiguras();
 IMedible figuraCuadrado = Factoria.Figura(EnumFiguras.Cuadrado, 2);
 IMedible figuraCirculo = Factoria.Figura(EnumFiguras.Circulo, 2);
+IMedible figuraCirculo2 = Factoria.Figura(EnumFiguras.Circulo, 3);
 
 
-Console.WriteLine(figuraCirculo);
+
 Console.WriteLine(figuraCuadrado);
+Console.WriteLine(figuraCirculo);
+Console.WriteLine(figuraCirculo2);
+
+var tablero01 = new TableroFiguras();
+tablero01.Add(figuraCuadrado);
+tablero01.Add(figuraCirculo);
+tablero01.Add(figuraCirculo2);
+
+
+Console.WriteLine($"Perimetro total: {tablero01.DamePerimetroTotal()}");
+Console.WriteLine($"Superficie total: {tablero01.DameSuperficieTotal()}");
+Console.WriteLine($"Total de objetos: {tablero01.Count()}");
+
+Console.WriteLine($"Media total Perimetro: {tablero01.DameMediaTotalPerimetro()}");
+Console.WriteLine($"Media total Superficie {tablero01.DameMediaTotalSuperficie()}");
+
+Console.WriteLine($"Superficie mas grande: {tablero01.DameSuperficieGrande()}");
+Console.WriteLine($"Superficie mas pequena: {tablero01.DameSuperficiePequena()}");
+
+
